c5a6c5fa3273371775326e3f50011c6340141371
gangsan yoon <gangsan.yoon@wsu.edu>
2022-12-09 01:53:07 +0900

    blog started, todo-> fix frontend

---
+++ server/blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=40)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # author = ...
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
+
+    
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}'
