4297ae31f3727e91281ae759dc7596c6bf445652
gangsan yoon <gangsan.yoon@wsu.edu>
2022-12-30 01:30:59 +0900

    tag underway

--- server/blog/admin.py
+++ server/blog/admin.py
@@ -6,4 +6,8 @@ admin.site.register(Post)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
 
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name', )}
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
