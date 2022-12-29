dac32346545f8163f66a724086cef99f4439668c
gangsan yoon <gangsan.yoon@wsu.edu>
2022-12-30 01:21:44 +0900

    category page completed

--- server/blog/tests.py
+++ server/blog/tests.py
@@ -196,3 +196,19 @@ class TestView(TestCase):
 
         about_me_btn = navbar.find('a', text='About Me')
         self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
+
+    def test_category_page(self):
+        response = self.client.get(self.category_one.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.navbar_test(soup)
+        self.category_card_test(soup)
+       
+        self.assertIn(self.category_one.name, soup.text)
+
+        main_area = soup.find('div', id='main-area')
+        self.assertIn(self.category_one.name, main_area.text)
+        self.assertIn(self.post_001.title, main_area.text)
+        self.assertNotIn(self.post_002.title, main_area.text) 
+        self.assertNotIn(self.post_003.title, main_area.text)
--- server/blog/urls.py
+++ server/blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     # path('', views.index),
     # path('<int:pk>/', views.single_post_page), 
     path('', views.PostList.as_view()),
-    path('<int:pk>/', views.PostDetail.as_view()), 
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('category/<str:slug>/', views.category_page),
 ]
